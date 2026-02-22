5da127a77d06df0ca5c2160bd543af02f1e40b12
liyuning <liyuning@guazi.com>
2026-02-22 21:41:31 +0800

    feat: 新增10首公版曲目及SEO元数据

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,17 @@ export default function Home() {
     { id: 'amazing-grace', title: 'Amazing Grace', key: 'C', tempo: 80 },
     { id: 'mary-lamb', title: 'Mary Had a Little Lamb', key: 'C', tempo: 110 },
     { id: 'jingle-bells', title: 'Jingle Bells', key: 'C', tempo: 130 },
-    { id: 'happy-birthday', title: 'Happy Birthday', key: 'C', tempo: 90 }
+    { id: 'happy-birthday', title: 'Happy Birthday', key: 'C', tempo: 90 },
+    { id: 'aura-lea', title: 'Aura Lea', key: 'C', tempo: 100 },
+    { id: 'auld-lang-syne', title: 'Auld Lang Syne', key: 'C', tempo: 100 },
+    { id: 'scarborough-fair', title: 'Scarborough Fair', key: 'C', tempo: 100 },
+    { id: 'greensleeves', title: 'Greensleeves', key: 'C', tempo: 100 },
+    { id: 'danny-boy', title: 'Danny Boy', key: 'C', tempo: 100 },
+    { id: 'sakura', title: 'Sakura Sakura', key: 'C', tempo: 100 },
+    { id: 'when-the-saints', title: 'When the Saints Go Marching In', key: 'C', tempo: 100 },
+    { id: 'you-are-my-sunshine', title: 'You Are My Sunshine', key: 'C', tempo: 100 },
+    { id: 'over-the-rainbow', title: 'Over the Rainbow', key: 'C', tempo: 100 },
+    { id: 'we-wish-you', title: 'We Wish You a Merry Christmas', key: 'C', tempo: 100 }
   ]
 
   return (
--- src/app/song/songData.ts
+++ src/app/song/songData.ts
@@ -45,7 +45,105 @@ T:Happy Birthday
 M:3/4
 L:1/4
 K:C
-G G A G C B | G G A G D C | G G G E C B A | F F E C D C |]`
+G G A G C B | G G A G D C | G G G E C B A | F F E C D C |]`,
+
+  'aura-lea': `X:1
+T:Aura Lea
+M:4/4
+L:1/4
+K:C
+C|C C D E|F3 F|E E F G|A3 A|
+G G A B|c3 c|B A G F|E3 E|
+C C D E|F3 F|E E F G|A3 A|
+G G A B|c3 c|B A G F|E3 E|`,
+
+  'auld-lang-syne': `X:1
+T:Auld Lang Syne
+M:4/4
+L:1/4
+K:C
+C|F F F E|F F F2 E|F F F E|F4 C|
+F F F E|F F F2 G|A A G F|C4|]`,
+
+  'scarborough-fair': `X:1
+T:Scarborough Fair
+M:3/4
+L:1/4
+K:C
+E|G2G|c2B|A2G|F2F|E3-|E2E|
+G2G|c2B|A2G|F2F|E3-|E2E|
+G2G|c2c|B2A|G2F|E3-|E2E|
+c2B|A2G|F2F|E2D|C3-|C2|`,
+
+  'greensleeves': `X:1
+T:Greensleeves
+M:6/8
+L:1/8
+K:C
+E|C2C G2G|A2A G2E|F2F E2D|C4 E|
+C2C G2G|A2A G2E|F2F E2D|C4 z|
+E|G2G G2G|A2B c2c|B2A G2E|F4 E|
+C2C G2G|A2A G2E|F2F E2D|C4|`,
+
+  'danny-boy': `X:1
+T:Danny Boy
+M:4/4
+L:1/4
+K:C
+C|D E F|G2 A|B c d|e3|
+e d c|B A G|F E D|C3|
+C|D E F|G2 A|B c d|e3|
+e d c|B A G|F E D|C3|`,
+
+  'sakura': `X:1
+T:Sakura Sakura
+M:3/4
+L:1/4
+K:C
+C|C D E|D C D|E F G|F3|
+E F G|A G A|B c d|c3|
+c B c|A G F|E G E|D3|
+C D E|D C D|E F G|F3|`,
+
+  'when-the-saints': `X:1
+T:When the Saints Go Marching In
+M:4/4
+L:1/4
+K:C
+C|E E E E|F F F F|E E E E|C4|
+E E E E|F F F F|E E E E|C4|
+C C C C|D D D D|E E E E|F4|
+E E E E|F F F F|E E E E|C4|]`,
+
+  'you-are-my-sunshine': `X:1
+T:You Are My Sunshine
+M:4/4
+L:1/4
+K:C
+C|C C C D|E3 C|C C C D|E3 C|
+C C E G|A3 G|F F E D|C3 C|
+C C C D|E3 C|C C C D|E3 C|
+C C E G|A3 G|F F E D|C3|]`,
+
+  'over-the-rainbow': `X:1
+T:Over the Rainbow
+M:4/4
+L:1/4
+K:C
+C|C C C C|E3 C|G G G G|A3 G|
+E E E E|F3 F|E E D D|C3 C|
+C C C C|E3 C|G G G G|A3 G|
+E E E E|F3 F|E D C2|C3|]`,
+
+  'we-wish-you': `X:1
+T:We Wish You a Merry Christmas
+M:3/4
+L:1/4
+K:C
+C|C C C|D D D|E E F|G3|
+G G G|A A A|G G F|E3|
+C C C|D D D|E E F|G3|
+G G G|A A A|G F E|C3|]`
 }
 
 export const songTitleMap: Record<string, string> = {
@@ -54,7 +152,17 @@ export const songTitleMap: Record<string, string> = {
   'amazing-grace': 'Amazing Grace',
   'mary-lamb': 'Mary Had a Little Lamb',
   'jingle-bells': 'Jingle Bells',
-  'happy-birthday': 'Happy Birthday'
+  'happy-birthday': 'Happy Birthday',
+  'aura-lea': 'Aura Lea',
+  'auld-lang-syne': 'Auld Lang Syne',
+  'scarborough-fair': 'Scarborough Fair',
+  'greensleeves': 'Greensleeves',
+  'danny-boy': 'Danny Boy',
+  'sakura': 'Sakura Sakura',
+  'when-the-saints': 'When the Saints Go Marching In',
+  'you-are-my-sunshine': 'You Are My Sunshine',
+  'over-the-rainbow': 'Over the Rainbow',
+  'we-wish-you': 'We Wish You a Merry Christmas'
 }
 
 export const songMetaMap: Record<string, { key: string; tempo: number }> = {
@@ -63,5 +171,15 @@ export const songMetaMap: Record<string, { key: string; tempo: number }> = {
   'amazing-grace': { key: 'C', tempo: 80 },
   'mary-lamb': { key: 'C', tempo: 110 },
   'jingle-bells': { key: 'C', tempo: 130 },
-  'happy-birthday': { key: 'C', tempo: 90 }
+  'happy-birthday': { key: 'C', tempo: 90 },
+  'aura-lea': { key: 'C', tempo: 100 },
+  'auld-lang-syne': { key: 'C', tempo: 100 },
+  'scarborough-fair': { key: 'C', tempo: 100 },
+  'greensleeves': { key: 'C', tempo: 100 },
+  'danny-boy': { key: 'C', tempo: 100 },
+  'sakura': { key: 'C', tempo: 100 },
+  'when-the-saints': { key: 'C', tempo: 100 },
+  'you-are-my-sunshine': { key: 'C', tempo: 100 },
+  'over-the-rainbow': { key: 'C', tempo: 100 },
+  'we-wish-you': { key: 'C', tempo: 100 }
 }
